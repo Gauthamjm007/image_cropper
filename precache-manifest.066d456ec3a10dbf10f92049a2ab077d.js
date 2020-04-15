ef928bdf722dfb9e00277cb2742e80487704ba9a
Gauthamjm007 <j.mgautham15@gmail.com>
2020-04-15 14:00:01 +0530

    Updates

---
+++ precache-manifest.066d456ec3a10dbf10f92049a2ab077d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2625fd33a89c03a45647b769d864a4b",
+    "url": "/image_cropper/index.html"
+  },
+  {
+    "revision": "a05c0bab2c2ed993d5f9",
+    "url": "/image_cropper/static/css/2.b9b7f290.chunk.css"
+  },
+  {
+    "revision": "f399a3eedc092d058a72",
+    "url": "/image_cropper/static/css/main.bf669c28.chunk.css"
+  },
+  {
+    "revision": "a05c0bab2c2ed993d5f9",
+    "url": "/image_cropper/static/js/2.b981ad90.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/image_cropper/static/js/2.b981ad90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f399a3eedc092d058a72",
+    "url": "/image_cropper/static/js/main.0900297e.chunk.js"
+  },
+  {
+    "revision": "6286e1999a7760d5c818",
+    "url": "/image_cropper/static/js/runtime-main.6e426a34.js"
+  }
+]);
